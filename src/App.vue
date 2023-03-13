bc8b3562c3f4ff6b2da678d943623b671442fc5d
palashkumar1508 <palashkumar1508@vidyamantra.com>
2023-03-13 17:47:10 +0530

    get Location By lat and Long

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import inputs from './inputs.vue';
+
+ 
+</script>
+
+<template>
+
+    <div class=" w-full flex flex-col gap-4 justify-center p-10 items-center rounded-lg m-4 mr-4">
+        
+            <div class="border-2 border-[#0ACFE8] shadow-md shadow-[#0ACFE8] flex flex-col gap-8 justify-center p-14 items-center rounded-lg bg-[#000000] text-2xl text-[#0ACFE8]">
+                <inputs />
+                
+            </div>
+    
+    </div>
+    
+</template>
+
+<style scoped>
+
+</style>
+
---
+++ src/inputs.vue
@@ -0,0 +1,56 @@
+<template>
+        <form action="" @submit.prevent="validation" >
+            <div class="flex flex-col gap-8 justify-center p-14 items-center rounded-lg bg-[#000000] text-2xl text-[#0ACFE8]">
+                <h1 class="text-7xl">Address</h1> 
+                <div>
+                    <label> Latitude : </label>
+                    <input class="" type="text" placeholder="Enter Latitude"  v-model="lat">
+                </div>
+                <div>
+                    <label>Longitude : </label>
+                    <input type="text" v-model="lon"  placeholder="Enter Longitude">
+                </div>
+                <button @click="getLoc" class="p-5 text-red-500 shadow-md shadow-[#0ACFE8] rounded-lg">Find</button>
+                <!-- <div v-if="!posts" class="text-white">{{ posts[0].display_name }}</div> -->
+                <h1 v-if="res">{{ res }}</h1>
+
+            </div>
+        </form>
+
+</template>
+
+<script setup>
+import {ref, onMounted}  from 'vue'
+import axios from 'axios'
+let lat = ref(null)
+let lon = ref(null)
+let res = ref('')
+
+/*
+onMounted(()=>{
+    axios.get(`https://nominatim.openstreetmap.org/search.php?q=${lat.value}%2C+${lon.value}&format=jsonv2`).then((response)=>{
+        posts.value = response.data
+    })
+})
+*/
+async function getLoc (){
+    // const response = await fetch(`https://nominatim.openstreetmap.org/search.php?q=${lat.value}%2C+${lon.value}&format=jsonv2`)
+    // const respondJSON = await response.json();
+    // console.log(respondJSON)
+    // res.value = respondJSON[0].display_name
+    axios.get(`https://nominatim.openstreetmap.org/search.php?q=${lat.value}%2C+${lon.value}&format=jsonv2`).then((response)=>{
+        res.value = response.data[0].display_name
+    })
+
+return;
+}
+
+</script>
+
+<style scoped>
+
+input{
+    @apply p-3 rounded-md m-4
+}
+
+</style>
